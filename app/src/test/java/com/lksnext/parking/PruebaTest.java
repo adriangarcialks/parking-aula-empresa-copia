4489031236ea2948bc9ba22de005e75ed0cedab2
adriangarcialks <101177266+adriangarcialks@users.noreply.github.com>
2023-01-25 16:11:02 +0100

    Create PruebaTest.java

---
+++ app/src/test/java/com/lksnext/parking/PruebaTest.java
@@ -0,0 +1,12 @@
+package com.lksnext.parking;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class PruebaTest {
+    @Test
+    public void TestSuma(){
+        assertEquals(4, 2 + 2);
+    }
+}
